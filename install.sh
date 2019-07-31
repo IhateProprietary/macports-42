e22ca3afa6764164333bb7e56e4fccfa2c5ddb6e
IhateProprietary <17804953+IhateProprietary@users.noreply.github.com>
2019-07-31 19:36:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ make install
 curl -fsSL https://raw.githubusercontent.com/IhateProprietary/macports-42/master/.macports.zsh > $HOME/.macportsconfig.zsh
 EXISTS=`grep "source ~/.macportsconfig.zsh" ~/.zshrc`
 if [[ -z $EXISTS ]]; then
-	echo "source ~/.macportsconfig.zsh" >> ~/.zshrc
+	echo "source ~/.macports.zsh" >> ~/.zshrc
 fi
 echo "Restart your terminal or alternative resource your zshrc to have effect immediately"
 echo "Don't forget to port selfupdate otherwise, it won't have any package to look for"
