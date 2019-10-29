ac605661835fe4543f741378107f937c85b1cb7d
IhateProprietary <17804953+IhateProprietary@users.noreply.github.com>
2019-10-29 18:30:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ mkdir -vp $TARGET
 make -j16
 make install
 
-curl -fsSL https://raw.githubusercontent.com/IhateProprietary/macports-42/master/.macports.zsh > $HOME/.macportsconfig.zsh
+curl -fsSL https://raw.githubusercontent.com/IhateProprietary/macports-42/master/.macports.zsh > $HOME/.macports.zsh
 EXISTS=`grep "source ~/.macportsconfig.zsh" ~/.zshrc`
 if [[ -z $EXISTS ]]; then
 	echo "source ~/.macports.zsh" >> ~/.zshrc
